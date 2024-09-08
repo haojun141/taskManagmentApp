f15efc7ad934028d066b1947c2a3c0930a012512
haojun14 <haojun0114@outlook.com>
2024-09-08 14:25:39 +0800

    replace old code with new code

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: [
+    "module:metro-react-native-babel-preset",
+    "@babel/preset-env",
+    "@babel/preset-react",
+  ],
 };
